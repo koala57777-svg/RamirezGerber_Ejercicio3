45021675f883616041e46f737e5d737ca10ab1ca
Gerber Ramírez <koala57777@gmail.com>
2026-02-21 19:22:40 -0600

    Se implementó la validación de datos, y la estrucutra del switch principal y secundario.

--- Ejercicio3-Sistema_Facturacion/Program.cs
+++ Ejercicio3-Sistema_Facturacion/Program.cs
@@ -21,11 +21,22 @@ Console.WriteLine("");
 Console.WriteLine("¿Tiene cupón? (S/N)");
 Console.WriteLine("");
 string tienecupon = Console.ReadLine();
-string codigo = "";
+bool cuponvalido = false;
 if (tienecupon == "S")
 {
-    Console.WriteLine("Ingrese el cupón: ");
-    codigo = Console.ReadLine();
+    Console.WriteLine("Ingrese la primera letra de su cupón: ");
+    string primeraLetra = Console.ReadLine();
+    Console.WriteLine("");
+    Console.WriteLine("Ingrese el último dígito de su cupón");
+    int ultimoDigito = int.Parse(Console.ReadLine());
+    if (primeraLetra == "U" && (ultimoDigito%2 == 0))
+    {
+        cuponvalido=true;
+    }
+    else
+    {
+        cuponvalido = false;
+    }
 }
 
 Console.WriteLine("");
@@ -37,6 +48,89 @@ Console.WriteLine("3. Pagos rechazados múltiples");
 Console.WriteLine("");
 int reporteFraude = int.Parse(Console.ReadLine());
 Console.WriteLine("");
+
+if (montoBase >= 0 && (tienecupon=="S" || tienecupon=="N"))
+{
+    switch (tipoCliente)
+    {
+        case 1:
+            switch(metodoPago)
+            {
+                case 1:
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                default:
+                    Console.WriteLine("Método de pago inválido.");
+                    return;
+            }
+            break;
+        case 2:
+            switch (metodoPago)
+            {
+                case 1:
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                default:
+                    Console.WriteLine("Método de pago inválido.");
+                    return;
+            }
+            break;
+        case 3:
+            switch (metodoPago)
+            {
+                case 1:
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                default:
+                    Console.WriteLine("Método de pago inválido.");
+                    return;
+            }
+            break;
+        case 4:
+            switch (metodoPago)
+            {
+                case 1:
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                default:
+                    Console.WriteLine("Método de pago inválido.");
+                    return;
+            }
+            break;
+        default:
+            Console.WriteLine("Tipo de cliente inválido.");
+            return;
+    }
+}
+else
+{
+    Console.WriteLine("Datos inválidos. Por favor ingrese datos válidos.");
+}
+
 
 
 
